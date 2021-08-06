447039652a6e0832b8f8b3881acf2c7f157e467c
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-05 21:42:55 -0500

    added a lot of js

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//creating 4 constant arrays with numbers, special characters, upper and lower case letters
 
 //Creates a password 
 function generatePassword(){
-  //need to generate a random number based on the length of an array.
-  //Need to make several arrays with lowercase and upper case letters, numbers, special characters
-  //Need to create an empty array thats chosen values in an array. 
+  //creating arrays
+  let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  let numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let special = ["!", "@", "#", "$", "%", "^", "&", "(", ")"];
+  let chosenValues = [];
+  let password = [];
+  
+  //Creates password length
+  let passLength = window.prompt("Please enter a number between 8 and 128.", 0);
+
+  //creates prompt do you want to include these arrays? 
+  let lower = window.confirm("Do you want to include lower case letters?");
+  if(lower){
+    chosenValues = [...lowerCase];
+  }
+  let upper = window.confirm("Do you want to include upper case letters?");
+  if(upper){
+    chosenValues = [...chosenValues, ...upperCase];
+  }
+  let num = window.confirm("Do you want to include numbers?");
+  if(num){
+    chosenValues = [...chosenValues, ...numeric];
+  }
+  let spec = window.confirm("Do you want to include special characters?");
+  if(spec){
+    chosenValues = [...chosenValues, ...special];
+  }
+
+  for(i = 0; i < passLength; i++){
+  let randomNumber = Math.floor(Math.random()*chosenValues.length);
+  password.push(chosenValues[randomNumber]);
+  }
   //How to add to an array let chosenValues = [...upper];
   //use a nested if statements to add to chosenValues
-  //Math.floor(Math.random()*chosenValues.lenth)
   //emptyArray needs to add chosenValues[Random number]
   //use .push to add to password 
   //password.join(""); adds to empty string
   // use for loop around repeated steps. 
   //need to return a string return "password";
-
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
